f135eafe19f643163bf922b3fd980612cf2f0d66
yashiyoshi <93021275+yashiyoshi@users.noreply.github.com>
2024-03-28 00:31:29 +0300

    Update Contact.tsx

--- app/Components/Contact.tsx
+++ app/Components/Contact.tsx
@@ -15,10 +15,10 @@ const Contact = () => {
       </span>
       <span className="tracking-wider items-start">DONT BE A STRANGER</span>
       <div className="opacity-50 font-normal text-2xl">
-        <p className="py-10">Let`&apos;`s make something exciting together!</p>
+        <p className="py-10">Let's make something exciting together!</p>
         <p className="py-10">
           If you have an opportunity or any project that would benefit from my
-          involvement, feel free to reach me at at any of my socials below. I`&apos;`ll
+          involvement, feel free to reach me at at any of my socials below. I'll
           be delighted to talk to you :)
         </p>
       </div>
