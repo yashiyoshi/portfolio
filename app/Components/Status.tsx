1c7277abbaf9741b89cee98536e3df1751ce907f
yashiyoshi <93021275+yashiyoshi@users.noreply.github.com>
2024-07-01 11:48:25 +0800

    Update Status.tsx

--- app/Components/Status.tsx
+++ app/Components/Status.tsx
@@ -4,8 +4,8 @@ const Status = () => {
   return (
     <div className="py-6 font-bold text-md sm:text-lg invisible md:visible opacity-0 animate-fade-in-delay-2">
       <span className="text-main2 opacity-50">current status</span>
-      <span className="text-4xl text-success absolute -translate-y-3.5 ml-1">.</span>
-      <p className="text-white opacity-80">-- available for work</p>
+      <span className="text-4xl text-fail absolute -translate-y-3.5 ml-1">.</span>
+      <p className="text-white opacity-80">-- interning at mugna tech</p>
       <p className="text-white opacity-20 text-sm font-normal invisible">applying for internships atm!</p>
       
     </div>
